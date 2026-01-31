2da42883d1bb5796d34322d6d6b2fdcc74d25a68
Marcinos7 <mkaminski8@outlook.com>
2026-01-31 20:50:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-import { 
-    getFirestore, 
-    collection, 
-    getDocs, 
-    doc, 
-    getDoc 
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 // KATEGORIE UWAG
 const KATEGORIE_UWAG = [
